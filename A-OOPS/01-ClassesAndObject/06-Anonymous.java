c8bc3229b9dce4a445d36996196e8bdfc0e5a2c1
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2023-01-23 00:44:46 +0530

    Anonymous Object

---
+++ A-OOPS/01-ClassesAndObject/06-Anonymous.java
@@ -0,0 +1,13 @@
+class A{
+    public A(){
+        System.out.println("Object Created");
+    }
+}
+
+
+public class Anonymous {
+    public static void main(String[] args) {
+        new A(); // This is Anonymous Object it is used only once
+
+    }
+}
