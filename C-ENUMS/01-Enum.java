9351972c892c9af5f274af89e9cd18d2dbf8a2df
GIRISHSHUKLAA <93051418+GIRISHSHUKLAA@users.noreply.github.com>
2023-02-13 18:02:33 +0530

    Enum Implementation

---
+++ C-ENUMS/01-Enum.java
@@ -0,0 +1,11 @@
+public class Main {
+   enum Beer {
+        KF, KO, RC, FO;
+    }
+    public static void main(String a[]) {
+        Beer b = Beer.FO;
+        System.out.println(b.toString());
+
+    }
+}
+
